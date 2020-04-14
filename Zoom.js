61576bf51ecfd65be340390075eb0a4763c95ba8
Veerendra <saiveerendra555@gmail.com>
2020-04-14 16:19:07 +0530

    firstapp

---
+++ Zoom.js
@@ -0,0 +1,12 @@
+ function zoom(ele)
+		{
+
+	    ele.style.height='300px';
+         ele.style.width='300px';
+        }
+
+ function normal(ele)
+    {
+    	ele.style.height="200px";
+    	 ele.style.width="200px";
+    }
